9917c7e0f01ddeaa28632bfa6a18a676203c5692
Amey Dhuri <ameydhuri08@gmail.com>
2026-08-18 12:49:51 +0530

    signup api

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -15,7 +16,32 @@ def create_app():
 
     @app.route("/")
     def home():
-        return {"message": "Learniee API is running!!"}
+        return{"message": "Learniee API is running!!"}
+
+    @app.route("/api/signup", methods=["POST"])
+    def signup():
+        data = request.get_json()
+
+        name = data.get("name")
+        email = data.get("email")
+        password = data.get("password")
+
+        if not name or not email or not password:
+            return{"error": "All field are required"}, 400
+
+        existing_user = User.query.filter_by(email=email).first()
+
+        if existing_user:
+            return{"error": "Email already registered!"}, 409
+
+        hashed_password = generate_password_hash(password)
+
+        user = User(name=name, email=email, password=hashed_password)
+
+        db.session.add(user)
+        db.session.commit()
+
+        return{"message": "Account created successfully!"}, 201
 
     with app.app_context():
         db.create_all()
